3cb6c5a154fe51963e3437277f3f3d0752f36ff1
victor conqueror <victorvector608@gmail.com>
2025-03-06 10:46:27 +0100

    update details

--- CONTROLLER/detailsController.js
+++ CONTROLLER/detailsController.js
@@ -31,6 +31,22 @@ const createUserDetails = async (req, res) => {
     }
 }
 
+// update details
+
+const updateDetails = async (req, res) => {
+    try{
+        const { Id } = req.params       
+        if(!Id){
+            return res.status(404).json({message:"user not found"})
+        }
+        const update = await detailsModel.findByIdAndUpdate(Id, req.body, {new:true});
+        return res.status(200).json({data:update});
+    }catch(err){
+        return res.status(500).json({message:"internal server error", error:err.message})
+    }
+}
+
 module.exports = {  
-    createUserDetails
+    createUserDetails,
+    updateDetails
 }
--- ROUTES/detailRoute.js
+++ ROUTES/detailRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const { createUserDetails } = require('../CONTROLLER/detailsController');
+const { createUserDetails, updateDetails } = require('../CONTROLLER/detailsController');
 
 const detailRouter = express.Router();
 
 detailRouter.post("/createDetail", createUserDetails);
+detailRouter.patch("/update/:Id",updateDetails)
 
 module.exports = detailRouter;
